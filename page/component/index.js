84454d79352682e7a553ec498538641f8f10caad
ghwq <459827999@qq.com>
2018-10-18 23:48:10 +0800

    Add files via upload

---
+++ page/component/index.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    imgUrls: [
+      'http://image.lingshi.com/data/afficheimg/1536797872191783523.jpg',
+      'http://image.lingshi.com/data/afficheimg/1537239284765876614.jpg',
+      'http://image.lingshi.com/data/afficheimg/1528165197450210615.jpg',
+      'http://image.lingshi.com/data/afficheimg/1517810945694990298.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
